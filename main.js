78778e1ce299397b6be2bf3bf4b864417dbe19d0
winterofchoi <87969359+winterofchoi@users.noreply.github.com>
2022-07-26 15:07:38 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.querySelector('navbar__toogleBtn');
-const menu = document.querySelector('navbar__menu');
-const icons = document.querySelector('navbar__icons');
+const toggleBtn = document.querySelector('.navbar__toogleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
